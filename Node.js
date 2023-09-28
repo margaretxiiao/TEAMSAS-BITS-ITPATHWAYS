2d428cc3ac05128ce934ab3d29b277570ea0af2c
margaretxiiao <80024003+margaretxiiao@users.noreply.github.com>
2023-09-29 01:44:26 +1000

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,20 +1,26 @@
-const MongoClient = require('mongodb').MongoClient;
 
-// Replace <username>, <password>, and <dbname> with your actual credentials and database name
-const uri = mongodb+srv:s3902846:<Penguin420!>@itpathwaysdbcluster.3y10hhc.mongodb.net/?retryWrites=true&w=majority;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s3902846:<Penguin420!>@itpathwaysdbcluster.3y10hhc.mongodb.net/?retryWrites=true&w=majority";
 
-
-// Create a MongoClient and connect to the MongoDB Atlas cluster
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect(err => {
-  if (err) {
-    console.error("Error connecting to MongoDB:", err);
-    return;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  console.log("Connected to MongoDB");
-  
-  // Now you can perform database operations here
-
-  // client.close(); //
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
