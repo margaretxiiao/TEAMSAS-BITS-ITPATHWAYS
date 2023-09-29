0419ba4fb71169d39c5201c87fc004a1071d3648
Siuuuuuuuuuuuuuuuuuuuu <103169394+Siuuuuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2023-09-29 18:00:44 +0700

    Update app.js

--- database/app.js
+++ database/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = process.env.PORT || 3000; 
+
 const mongoUrl = 'mongodb+srv://lekhanhtoan07:T14012003oan@server1.h0nl7gl.mongodb.net/'; 
 const dbName = 'Web'; 
 const collectionName = 'Test'; 
