78dc3e4631e85ebae29557c6cb01fe6ba0308eec
Siuuuuuuuuuuuuuuuuuuuu <103169394+Siuuuuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2023-09-29 10:35:08 +0700

    Update app.js

--- database/app.js
+++ database/app.js
@@ -24,7 +24,7 @@ app.post('/register', async (req, res) => {
         // Check if email or username already exist
         const existingUser = await collection.findOne({ $or: [{ email }, { username },{ phone }] });
         if (existingUser) {
-            return res.send('Email or username or phone number already exists.');
+            return res.send('failed.html');
         }
 
         // Insert the new user into the database
